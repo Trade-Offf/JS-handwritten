16bb790729d1111f7bc9fe52b25c3a1a6dd906fb
Trade-Offf <trade_offf@163.com>
2024-09-17 19:20:32 +0800

    update 注释

--- 手写题-功能概念/防抖.js
+++ 手写题-功能概念/防抖.js
@@ -1,13 +1,13 @@
 // 逻辑：在事件被触发 n 秒后再执行回调，如果在这 n 秒内又被触发，则重新计时。
 // 使用场景：输入框输入监听事件、滚动事件、按钮点击等。
 function debounce(func, wait) {
-    let timeout;
+    let timeId;
 
     return function (...args) {
-        clearTimeout(timeout);	// 清除上次的定时器
+        clearTimeout(timeId);	// 清除上次的定时器
 
         // 新建本次的定时器，并在 wait 毫秒后执行回调
-        timeout = setTimeout(() => {
+        timeId = setTimeout(() => {
             func.apply(this, args);
         }, wait);
     };
